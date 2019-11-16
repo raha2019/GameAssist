da183b35116d0a82d743d5ab2a121b32248ee87a
Rahul Gupta <rguptaemail10@gmail.com>
2019-11-16 15:26:09 -0500

    testing for four squares

--- Experimental/experimental.py
+++ Experimental/experimental.py
@@ -1,16 +1,20 @@
+#from gtts import gTTs
 import cv2
 import numpy as np
+import image_slicer
+from playsound import playsound
+import time
 
-def order66():
-    backgroundImage = "PipeTest.png"
-    luckbox1(backgroundImage)
-    luckbox2(backgroundImage)
-    luckbox3(backgroundImage)
-    poop1(backgroundImage)
-    poop2(backgroundImage)
-    pipe(backgroundImage)
+def order66(backgroundImage):
+    firstluckbox1(backgroundImage)
+    firstluckbox2(backgroundImage)
+    firstluckbox3(backgroundImage)
+    firstpoop1(backgroundImage)
+    firstpoop2(backgroundImage)
+    firstpipe(backgroundImage)
 
-def luckbox1(backgroundImage):
+def firstluckbox1(backgroundImage):
+    backgroundImage = 'REEE_01_01.png'
     img_rgb = cv2.imread(backgroundImage)
     img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
     template = cv2.imread('luckybox1.png',0)
@@ -20,9 +24,11 @@ def luckbox1(backgroundImage):
     loc = np.where( res >= threshold)
     for pt in zip(*loc[::-1]):
         cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,255,255), 2)
+
     cv2.imwrite('resultluckbox1.png',img_rgb)
 
-def luckbox2(backgroundImage):
+def firstluckbox2(backgroundImage):
+    backgroundImage = 'REEE_01_01.png'
     img_rgb = cv2.imread(backgroundImage)
     img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
     template = cv2.imread('luckybox2.png',0)
@@ -34,7 +40,8 @@ def luckbox2(backgroundImage):
         cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,255,255), 2)
     cv2.imwrite('resultluckbox2.png',img_rgb)
 
-def luckbox3(backgroundImage):
+def firstluckbox3(backgroundImage):
+    backgroundImage = 'REEE_01_01.png'
     img_rgb = cv2.imread(backgroundImage)
     img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
     template = cv2.imread('luckybox3.png',0)
@@ -46,7 +53,8 @@ def luckbox3(backgroundImage):
         cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,255,255), 2)
     cv2.imwrite('resultluckbox3.png',img_rgb)
 
-def poop1(backgroundImage):
+def firstpoop1(backgroundImage):
+    backgroundImage = 'REEE_01_01.png'
     img_rgb = cv2.imread(backgroundImage)
     img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
 
@@ -61,7 +69,8 @@ def poop1(backgroundImage):
         cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,255,255), 2)
     cv2.imwrite('resultpoop1.png',img_rgb)
 
-def poop2(backgroundImage):
+def firstpoop2(backgroundImage):
+    backgroundImage = 'REEE_01_01.png'
     img_rgb = cv2.imread(backgroundImage)
     img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
     template = cv2.imread('poop2.png',0)
@@ -73,7 +82,8 @@ def poop2(backgroundImage):
         cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,255,255), 2)
     cv2.imwrite('resultpoop2.png',img_rgb)
 
-def pipe(backgroundImage):
+def firstpipe(backgroundImage):
+    backgroundImage = 'REEE_01_01.png'
     img_rgb = cv2.imread(backgroundImage)
     img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
     template = cv2.imread('pipe.png',0)
@@ -85,4 +95,287 @@ def pipe(backgroundImage):
         cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,255,255), 2)
     cv2.imwrite('resultpipe.png',img_rgb)
 
-order66()
+
+def order69(backgroundImage):
+    backgroundImage = 'REEE_01_02.png'
+    secondluckbox1(backgroundImage)
+    secondluckbox2(backgroundImage)
+    secondluckbox3(backgroundImage)
+    secondpoop1(backgroundImage)
+    secondpoop2(backgroundImage)
+    secondpipe(backgroundImage)
+
+def secondluckbox1(backgroundImage):
+    backgroundImage = 'REEE_01_02.png'
+    img_rgb = cv2.imread(backgroundImage)
+    img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+    template = cv2.imread('luckybox1.png',0)
+    w, h = template.shape[::-1]
+    res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+    threshold = 0.5
+    loc = np.where( res >= threshold)
+    for pt in zip(*loc[::-1]):
+        cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,255,255), 2)
+    cv2.imwrite('resultluckbox1.png',img_rgb)
+
+def secondluckbox2(backgroundImage):
+    backgroundImage = 'REEE_01_02.png'
+    img_rgb = cv2.imread(backgroundImage)
+    img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+    template = cv2.imread('luckybox2.png',0)
+    w, h = template.shape[::-1]
+    res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+    threshold = 0.5
+    loc = np.where( res >= threshold)
+    for pt in zip(*loc[::-1]):
+        cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,255,255), 2)
+    cv2.imwrite('resultluckbox2.png',img_rgb)
+
+def secondluckbox3(backgroundImage):
+    backgroundImage = 'REEE_01_02.png'
+    img_rgb = cv2.imread(backgroundImage)
+    img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+    template = cv2.imread('luckybox3.png',0)
+    w, h = template.shape[::-1]
+    res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+    threshold = 0.5
+    loc = np.where( res >= threshold)
+    for pt in zip(*loc[::-1]):
+        cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,255,255), 2)
+    cv2.imwrite('resultluckbox3.png',img_rgb)
+
+def secondpoop1(backgroundImage):
+    backgroundImage = 'REEE_01_02.png'
+    img_rgb = cv2.imread(backgroundImage)
+    img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+
+    template = cv2.imread('poop1.png',0)
+    w, h = template.shape[::-1]
+
+    res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+    threshold = 0.45
+    loc = np.where( res >= threshold)
+
+    for pt in zip(*loc[::-1]):
+        cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,255,255), 2)
+    cv2.imwrite('resultpoop1.png',img_rgb)
+
+def secondpoop2(backgroundImage):
+    backgroundImage = 'REEE_01_02.png'
+    img_rgb = cv2.imread(backgroundImage)
+    img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+    template = cv2.imread('poop2.png',0)
+    w, h = template.shape[::-1]
+    res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+    threshold = 0.45
+    loc = np.where( res >= threshold)
+    for pt in zip(*loc[::-1]):
+        cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,255,255), 2)
+    cv2.imwrite('resultpoop2.png',img_rgb)
+
+def secondpipe(backgroundImage):
+    backgroundImage = 'REEE_01_02.png'
+    img_rgb = cv2.imread(backgroundImage)
+    img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+    template = cv2.imread('pipe.png',0)
+    w, h = template.shape[::-1]
+    res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+    threshold = 0.3
+    loc = np.where( res >= threshold)
+    for pt in zip(*loc[::-1]):
+        cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,255,255), 2)
+    cv2.imwrite('resultpipe.png',img_rgb)
+
+def order420(backgroundImage):
+    # backgroundImage = "PipeTest.png"
+    backgroundImage = 'REEE_02_01.png'
+    thirdluckbox1(backgroundImage)
+    thirdluckbox2(backgroundImage)
+    thirdluckbox3(backgroundImage)
+    thirdpoop1(backgroundImage)
+    thirdpoop2(backgroundImage)
+    thirdpipe(backgroundImage)
+
+def thirdluckbox1(backgroundImage):
+    backgroundImage = 'REEE_02_01.png'
+    img_rgb = cv2.imread(backgroundImage)
+    img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+    template = cv2.imread('luckybox1.png',0)
+    w, h = template.shape[::-1]
+    res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+    threshold = 0.5
+    loc = np.where( res >= threshold)
+    for pt in zip(*loc[::-1]):
+        cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,255,255), 2)
+    cv2.imwrite('resultluckbox1.png',img_rgb)
+
+def thirdluckbox2(backgroundImage):
+    backgroundImage = 'REEE_02_01.png'
+    img_rgb = cv2.imread(backgroundImage)
+    img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+    template = cv2.imread('luckybox2.png',0)
+    w, h = template.shape[::-1]
+    res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+    threshold = 0.5
+    loc = np.where( res >= threshold)
+    for pt in zip(*loc[::-1]):
+        cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,255,255), 2)
+    cv2.imwrite('resultluckbox2.png',img_rgb)
+
+def thirdluckbox3(backgroundImage):
+    backgroundImage = 'REEE_02_01.png'
+    img_rgb = cv2.imread(backgroundImage)
+    img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+    template = cv2.imread('luckybox3.png',0)
+    w, h = template.shape[::-1]
+    res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+    threshold = 0.5
+    loc = np.where( res >= threshold)
+    for pt in zip(*loc[::-1]):
+        cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,255,255), 2)
+    cv2.imwrite('resultluckbox3.png',img_rgb)
+
+def thirdpoop1(backgroundImage):
+    backgroundImage = 'REEE_02_01.png'
+    img_rgb = cv2.imread(backgroundImage)
+    img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+
+    template = cv2.imread('poop1.png',0)
+    w, h = template.shape[::-1]
+
+    res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+    threshold = 0.45
+    loc = np.where( res >= threshold)
+
+    for pt in zip(*loc[::-1]):
+        cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,255,255), 2)
+    cv2.imwrite('resultpoop1.png',img_rgb)
+
+def thirdpoop2(backgroundImage):
+    backgroundImage = 'REEE_02_01.png'
+    img_rgb = cv2.imread(backgroundImage)
+    img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+    template = cv2.imread('poop2.png',0)
+    w, h = template.shape[::-1]
+    res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+    threshold = 0.45
+    loc = np.where( res >= threshold)
+    for pt in zip(*loc[::-1]):
+        cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,255,255), 2)
+    cv2.imwrite('resultpoop2.png',img_rgb)
+
+def thirdpipe(backgroundImage):
+    backgroundImage = 'REEE_02_01.png'
+    img_rgb = cv2.imread(backgroundImage)
+    img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+    template = cv2.imread('pipe.png',0)
+    w, h = template.shape[::-1]
+    res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+    threshold = 0.3
+    loc = np.where( res >= threshold)
+    for pt in zip(*loc[::-1]):
+        cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,255,255), 2)
+    cv2.imwrite('resultpipe.png',img_rgb)
+
+def order69420(backgroundImage):
+    # backgroundImage = "PipeTest.png"
+    backgroundImage = 'REEE_02_02.png'
+    fourthluckbox1(backgroundImage)
+    fourthluckbox2(backgroundImage)
+    fourthluckbox3(backgroundImage)
+    fourthpoop1(backgroundImage)
+    fourthpoop2(backgroundImage)
+    fourthpipe(backgroundImage)
+
+def fourthluckbox1(backgroundImage):
+    backgroundImage = 'REEE_02_02.png'
+    img_rgb = cv2.imread(backgroundImage)
+    img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+    template = cv2.imread('luckybox1.png',0)
+    w, h = template.shape[::-1]
+    res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+    threshold = 0.5
+    loc = np.where( res >= threshold)
+    for pt in zip(*loc[::-1]):
+        cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,255,255), 2)
+    cv2.imwrite('resultluckbox1.png',img_rgb)
+
+def fourthluckbox2(backgroundImage):
+    backgroundImage = 'REEE_02_02.png'
+    img_rgb = cv2.imread(backgroundImage)
+    img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+    template = cv2.imread('luckybox2.png',0)
+    w, h = template.shape[::-1]
+    res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+    threshold = 0.5
+    loc = np.where( res >= threshold)
+    for pt in zip(*loc[::-1]):
+        cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,255,255), 2)
+    cv2.imwrite('resultluckbox2.png',img_rgb)
+
+def fourthluckbox3(backgroundImage):
+    backgroundImage = 'REEE_02_02.png'
+    img_rgb = cv2.imread(backgroundImage)
+    img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+    template = cv2.imread('luckybox3.png',0)
+    w, h = template.shape[::-1]
+    res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+    threshold = 0.5
+    loc = np.where( res >= threshold)
+    for pt in zip(*loc[::-1]):
+        cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,255,255), 2)
+    cv2.imwrite('resultluckbox3.png',img_rgb)
+
+def fourthpoop1(backgroundImage):
+    backgroundImage = 'REEE_02_02.png'
+    img_rgb = cv2.imread(backgroundImage)
+    img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+
+    template = cv2.imread('poop1.png',0)
+    w, h = template.shape[::-1]
+
+    res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+    threshold = 0.45
+    loc = np.where( res >= threshold)
+
+    for pt in zip(*loc[::-1]):
+        cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,255,255), 2)
+    cv2.imwrite('resultpoop1.png',img_rgb)
+
+def fourthpoop2(backgroundImage):
+    backgroundImage = 'REEE_02_02.png'
+    img_rgb = cv2.imread(backgroundImage)
+    img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+    template = cv2.imread('poop2.png',0)
+    w, h = template.shape[::-1]
+    res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+    threshold = 0.45
+    loc = np.where( res >= threshold)
+    for pt in zip(*loc[::-1]):
+        cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,255,255), 2)
+    cv2.imwrite('resultpoop2.png',img_rgb)
+
+def fourthpipe(backgroundImage):
+    backgroundImage = 'REEE_02_02.png'
+    img_rgb = cv2.imread(backgroundImage)
+    img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+    template = cv2.imread('pipe.png',0)
+    w, h = template.shape[::-1]
+    res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+    threshold = 0.3
+    loc = np.where( res >= threshold)
+    for pt in zip(*loc[::-1]):
+        cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,255,255), 2)
+    cv2.imwrite('resultpipe.png',img_rgb)
+
+while True:
+    time.sleep(10)
+    image_slicer.slice('REEE.png', 4)
+    backgroundImage = 'REEE_01_01.png'
+    order66(backgroundImage)
+    backgroundImage = 'REEE_01_02.png'
+    order69(backgroundImage)
+    backgroundImage = 'REEE_02_01.png'
+    order66(backgroundImage)
+    backgroundImage = 'REEE_02_02.png'
+    order69420(backgroundImage)
